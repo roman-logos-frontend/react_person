78798b3c96dc114c74aef1f32168845dd99cb88f
roman-logos-frontend <romanst235@gmail.com>
2024-07-15 18:25:38 +0300

    Added mentor corrections

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,14 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">My name is {person.name}</h2>
-    {person.age === undefined ? null : (
-      <p className="Person__age">I am {person.age}</p>
-    )}
-    {person.isMarried === false ? (
-      <p className="Person__partner">I am not married</p>
-    ) : (
-      <p className="Person__partner">
-        {person.partnerName} is my {person.sex === 'm' ? 'wife' : 'husband'}
-      </p>
-    )}
-  </section>
-);
+export const Person = ({ person }) => {
+  const personPartnerType = person.sex === 'm' ? 'wife' : 'husband';
+  const personPartner = person.isMarried
+    ? `${person.partnerName} is my ${personPartnerType}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {person.age && <p className="Person__age">I am {person.age}</p>}
+      <p className="Person__partner">{personPartner}</p>
+    </section>
+  );
+};
